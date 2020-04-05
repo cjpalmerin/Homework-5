c855090d21c62c5b64d3f6566d49d0da168672e9
CJPALME <cjpalmerin@gmail.com>
2020-04-04 19:17:20 -0500

    Final homework product

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# READ ME
+
+This application displays the current date and time inside of the jumbotron. From there you can see what time of day it is based on the coloring of the different rows in the application. Grey rows will show when that time of day has already passed, a red row will show you the current hour slot, and green rows will show the future. Users can input their own data into the rows and, through local storage, can see their inputs even after leaving or refreshing the page.
+
+The first task I took on was the clock. Knowing from the demo I immediately tried figuring out how the time worked before realizing we had to use a library. I had existing code using the Date function but used Moment.js to work how to display the time in the jumbotron. From there I was able to figure out how to change the color of the rows based on the hour of the day by using if else statements. The if else statements keep track of whether or not the current time is past, present, or future in relation to the current time of day.
+
+Next I stored every value from the text areas and the buttons into variables that would be used during onclick events to trigger the setItem and getItem functions related to local storage. These functions are what behind the persistance of the app.
+
+
+
 # 05 Third-Party APIs: Work Day Scheduler
 
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
--- script.js
+++ script.js
@@ -114,82 +114,82 @@ else if (hourNow < 17) {
     fivePM.attr("class", "future");
 }
 
-var sixPM = $("#sixPM"); 
-if (hourNow > 18) {
-    sixPM.attr("class", "past");
-}
-else if (hourNow == 18) {
-    sixPM.attr("class", "present");
-}
-else if (hourNow < 18) {
-    sixPM.attr("class", "future");
-}
+// var sixPM = $("#sixPM"); 
+// if (hourNow > 18) {
+//     sixPM.attr("class", "past");
+// }
+// else if (hourNow == 18) {
+//     sixPM.attr("class", "present");
+// }
+// else if (hourNow < 18) {
+//     sixPM.attr("class", "future");
+// }
 
-var sevenPM = $("#sevenPM"); 
-if (hourNow > 19) {
-    sevenPM.attr("class", "past");
-}
-else if (hourNow == 19) {
-    sevenPM.attr("class", "present");
-}
-else if (hourNow < 19) {
-    sevenPM.attr("class", "future");
-}
+// var sevenPM = $("#sevenPM"); 
+// if (hourNow > 19) {
+//     sevenPM.attr("class", "past");
+// }
+// else if (hourNow == 19) {
+//     sevenPM.attr("class", "present");
+// }
+// else if (hourNow < 19) {
+//     sevenPM.attr("class", "future");
+// }
 
-var eightPM = $("#eightPM"); 
-if (hourNow > 20) {
-    eightPM.attr("class", "past");
-}
-else if (hourNow == 20) {
-    eightPM.attr("class", "present");
-}
-else if (hourNow < 20) {
-    eightPM.attr("class", "future");
-}
+// var eightPM = $("#eightPM"); 
+// if (hourNow > 20) {
+//     eightPM.attr("class", "past");
+// }
+// else if (hourNow == 20) {
+//     eightPM.attr("class", "present");
+// }
+// else if (hourNow < 20) {
+//     eightPM.attr("class", "future");
+// }
 
-var ninePM = $("#ninePM"); 
-if (hourNow > 21) {
-    ninePM.attr("class", "past");
-}
-else if (hourNow == 21) {
-    ninePM.attr("class", "present");
-}
-else if (hourNow < 21) {
-    ninePM.attr("class", "future");
-}
+// var ninePM = $("#ninePM"); 
+// if (hourNow > 21) {
+//     ninePM.attr("class", "past");
+// }
+// else if (hourNow == 21) {
+//     ninePM.attr("class", "present");
+// }
+// else if (hourNow < 21) {
+//     ninePM.attr("class", "future");
+// }
 
-var tenPM = $("#tenPM"); 
-if (hourNow > 22) {
-    tenPM.attr("class", "past");
-}
-else if (hourNow == 22) {
-    tenPM.attr("class", "present");
-}
-else if (hourNow < 22) {
-    tenPM.attr("class", "future");
-}
+// var tenPM = $("#tenPM"); 
+// if (hourNow > 22) {
+//     tenPM.attr("class", "past");
+// }
+// else if (hourNow == 22) {
+//     tenPM.attr("class", "present");
+// }
+// else if (hourNow < 22) {
+//     tenPM.attr("class", "future");
+// }
 
-var elevenPM = $("#elevenPM"); 
-if (hourNow > 23) {
-    elevenPM.attr("class", "past");
-}
-else if (hourNow == 23) {
-    elevenPM.attr("class", "present");
-}
-else if (hourNow < 23) {
-    elevenPM.attr("class", "future");
-}
+// var elevenPM = $("#elevenPM"); 
+// if (hourNow > 23) {
+//     elevenPM.attr("class", "past");
+// }
+// else if (hourNow == 23) {
+//     elevenPM.attr("class", "present");
+// }
+// else if (hourNow < 23) {
+//     elevenPM.attr("class", "future");
+// }
 
-var twelveAM = $("#twelveAM"); 
-if (hourNow > 24) {
-    twelveAM.attr("class", "past");
-}
-else if (hourNow == 24) {
-    twelveAM.attr("class", "present");
-}
-else if (hourNow < 24) {
-    twelveAM.attr("class", "future");
-}
+// var twelveAM = $("#twelveAM"); 
+// if (hourNow > 24) {
+//     twelveAM.attr("class", "past");
+// }
+// else if (hourNow == 24) {
+//     twelveAM.attr("class", "present");
+// }
+// else if (hourNow < 24) {
+//     twelveAM.attr("class", "future");
+// }
 
 // render1();
 
@@ -279,4 +279,98 @@ subButton4.on('click', function () {
         userSubmit4 = userInput4.val();
         console.log(userSubmit4);
         localStorage.setItem("userInput4", userSubmit4);
+    });
+
+
+
+var userInput5 = $("#userInput5");
+var subButton5 = $('.subButton5'); 
+
+function render5 () {
+    var renderInput5 = localStorage.getItem("userInput5");
+    console.log(renderInput5);
+    userInput5.text(renderInput5);
+ }
+
+render5();
+
+subButton5.on('click', function () {
+        userSubmit5 = userInput5.val();
+        console.log(userSubmit5);
+        localStorage.setItem("userInput5", userSubmit5);
+    });
+
+
+
+var userInput6 = $("#userInput6");
+var subButton6 = $('.subButton6'); 
+
+function render6 () {
+    var renderInput6 = localStorage.getItem("userInput6");
+    console.log(renderInput6);
+    userInput6.text(renderInput6);
+ }
+
+render6();
+
+subButton6.on('click', function () {
+        userSubmit6 = userInput6.val();
+        console.log(userSubmit6);
+        localStorage.setItem("userInput6", userSubmit6);
+    });
+
+
+
+var userInput7 = $("#userInput7");
+var subButton7 = $('.subButton7'); 
+
+function render7 () {
+    var renderInput7 = localStorage.getItem("userInput7");
+    console.log(renderInput7);
+    userInput7.text(renderInput7);
+ }
+
+render7();
+
+subButton7.on('click', function () {
+        userSubmit7 = userInput7.val();
+        console.log(userSubmit7);
+        localStorage.setItem("userInput7", userSubmit7);
+    });
+
+
+var userInput8 = $("#userInput8");
+var subButton8 = $('.subButton8'); 
+
+function render8 () {
+    var renderInput8 = localStorage.getItem("userInput8");
+    console.log(renderInput8);
+    userInput8.text(renderInput8);
+ }
+
+render8();
+
+subButton8.on('click', function () {
+        userSubmit8 = userInput8.val();
+        console.log(userSubmit8);
+        localStorage.setItem("userInput8", userSubmit8);
+    });
+
+
+
+var userInput9 = $("#userInput9");
+var subButton9 = $('.subButton9'); 
+
+function render9 () {
+    var renderInput9 = localStorage.getItem("userInput9");
+    console.log(renderInput9);
+    userInput9.text(renderInput9);
+ }
+
+render9();
+
+subButton9.on('click', function () {
+        userSubmit9 = userInput9.val();
+        console.log(userSubmit9);
+        localStorage.setItem("userInput9", userSubmit9);
     });
